9ed7c76af7f885b8f0986500ae25c11df4e89751
dcz <dcz@phpbb-seo.com>
2009-09-30 17:21:34 +0000

    SVN: fix svn:keywords part2

--- contrib/redirect.php
+++ contrib/redirect.php
@@ -1,7 +1,10 @@
 <?php
 /**
 *
-* redirect.php
+* @package Ultimate SEO URL phpBB SEO
+* @version $Id$
+* @copyright (c) 2006 - 2009 www.phpbb-seo.com
+* @license http://www.opensource.org/licenses/rpl1.5.txt Reciprocal Public License 1.5
 *
 */
 // Some config
