976f84e5877ed71271b646037ac143dfb862a374
dcz <dcz@phpbb-seo.com>
2009-12-20 12:04:05 +0000

    RELATED : En language pack fixes from sevenalive (phpBB SEO user)

--- premod/root/language/en/mods/phpbb_seo_related_install.php
+++ premod/root/language/en/mods/phpbb_seo_related_install.php
@@ -35,7 +35,7 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	// ACP
 	'SEO_RELATED' => 'Related topics activation',
-	'SEO_RELATED_EXPLAIN' => 'Display or not a related topic list in topic pages.<br/><b style="color:red;">Note :</b><br/>With MYSQL >=4.1 and the topic table using MyISAM, related topics will be obtained using a FullText index on  the topic title and will be sorted by relevancy. in other cases, an SQL LIKE will be used, and results will be sorted by publication time',
+	'SEO_RELATED_EXPLAIN' => 'Display or not a related topic list in topic pages.<br/><b style="color:red;">Note :</b><br/>With mySQL >=4.1 and the topic table using MyISAM, related topics will be obtained using a FullText index on  the topic title and will be sorted by relevancy. in other cases, an SQL LIKE will be used, and results will be sorted by publication time',
 	'SEO_RELATED_CHECK_IGNORE' => 'Ignore words filter',
 	'SEO_RELATED_CHECK_IGNORE_EXPLAIN' => 'Apply, or not, the search_ignore_words.php exclusions while searching for related topics',
 	'SEO_RELATED_LIMIT' => 'Related topics limit',
@@ -45,16 +45,16 @@ $lang = array_merge($lang, array(
 	// Install
 	'INSTALLED' => 'phpBB SEO Related Topics mod installed',
 	'ALREADY_INSTALLED' => 'phpBB SEO Related Topics mod is already installed',
-	'FULLTEXT_INSTALLED' => 'Mysql FullText Index installed',
-	'FULLTEXT_NOT_INSTALLED' => 'Mysql FullText Index is not available on this server, SQL LIKE will be used instead',
+	'FULLTEXT_INSTALLED' => 'MySQL FullText Index installed',
+	'FULLTEXT_NOT_INSTALLED' => 'MySQL FullText Index is not available on this server, SQL LIKE will be used instead',
 	'INSTALLATION' => 'phpBB SEO Related Topics mod installation',
-	'INSTALLATION_START' => '&rArr; <a href="%1$s" ><b>Proceed with installing the mod</b></a><br/><br/>&rArr; <a href="%2$s" ><b>Retry to set the FullText Index</b></a> (Mysql >= 4.1 using Myisam for topic table only)<br/><br/>&rArr; <a href="%3$s" ><b>Proceed with un-installing the mod</b></a>',
+	'INSTALLATION_START' => '&rArr; <a href="%1$s" ><b>Proceed with installing the mod</b></a><br/><br/>&rArr; <a href="%2$s" ><b>Retry to set the FullText Index</b></a> (MySQL >= 4.1 using Myisam for topic table only)<br/><br/>&rArr; <a href="%3$s" ><b>Proceed with un-installing the mod</b></a>',
 	// un-install
 	'UNINSTALLED' => 'phpBB SEO Related Topics mod un-installed',
 	'ALREADY_UNINSTALLED' => 'phpBB SEO Related Topics mod is already un-installed',
 	'UNINSTALLATION' => 'phpBB SEO Related Topics mod un-installation',
 	// SQL message
-	'SQL_REQUIRED' => 'The configured db user does not have enough priviledges to alter tables, you need to run this query manually in order to add or drop the Mysql FullText index :<br/>%1$s',
+	'SQL_REQUIRED' => 'The configured db user does not have enough privileges to alter tables, you need to run this query manually in order to add or drop the MySQL FullText index :<br/>%1$s',
 	// Security
 	'SEO_LOGIN'		=> 'The board requires you to be registered and logged in to view this page.',
 	'SEO_LOGIN_ADMIN'	=> 'The board requires you to be logged in as admin to view this page.<br/>Your session has been destroyed for security purposes.',
