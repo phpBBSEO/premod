574084e161c8145cfb340a1e3ea0879d830d4e64
dcz <dcz@phpbb-seo.com>
2011-05-04 19:09:30 +0000

    NO DUPE : Fix last topic link could use wrong forum with virtual folders and sql rewriting off

--- premod/root/includes/functions_display.php
+++ premod/root/includes/functions_display.php
@@ -90,7 +90,7 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 		$sql_array['SELECT'] .= ', fa.user_id';
 	}
 	// www.phpBB-SEO.com SEO TOOLKIT BEGIN -> no dupe
-	if (@$phpbb_seo->seo_opt['no_dupe']['on']) {
+	if (!empty($phpbb_seo->seo_opt['no_dupe']['on'])) {
 		$sql_array['SELECT'] .= ', t.topic_id, t.topic_title, t.topic_replies, t.topic_replies_real, t.topic_status, t.topic_type, t.topic_moved_id' . (!empty($phpbb_seo->seo_opt['sql_rewrite']) ? ', t.topic_url ' : ' ');
 		$sql_array['LEFT_JOIN'][] = array(
 			'FROM'	=> array(TOPICS_TABLE => 't'),
@@ -182,7 +182,7 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 		// Count the difference of real to public topics, so we can display an information to moderators
 		$row['forum_id_unapproved_topics'] = ($auth->acl_get('m_approve', $forum_id) && ($row['forum_topics_real'] != $row['forum_topics'])) ? $forum_id : 0;
 		// www.phpBB-SEO.com SEO TOOLKIT BEGIN -> no dupe
-		if (@$phpbb_seo->seo_opt['no_dupe']['on']) {
+		if (!empty($phpbb_seo->seo_opt['no_dupe']['on'])) {
 			if ($row['topic_status'] == ITEM_MOVED) {
 				$row['topic_id'] = $row['topic_moved_id'];
 			}
@@ -281,7 +281,7 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 				$forum_rows[$parent_id]['forum_last_poster_colour'] = $row['forum_last_poster_colour'];
 				$forum_rows[$parent_id]['forum_id_last_post'] = $forum_id;
 				// www.phpBB-SEO.com SEO TOOLKIT BEGIN -> no dupe
-				if (@$phpbb_seo->seo_opt['no_dupe']['on']) {
+				if (!empty($phpbb_seo->seo_opt['no_dupe']['on'])) {
 					$forum_rows[$parent_id]['topic_id'] = $row['topic_id'];
 					$forum_rows[$parent_id]['topic_title'] = $row['topic_title'];
 					$forum_rows[$parent_id]['topic_type'] = $row['topic_type'];
@@ -445,8 +445,8 @@ function display_forums($root_data = '', $display_moderators = true, $return_mod
 			$last_post_subject = $row['forum_last_post_subject'];
 			$last_post_time = $user->format_date($row['forum_last_post_time']);
 			// www.phpBB-SEO.com SEO TOOLKIT BEGIN -> no dupe
-			if (@$phpbb_seo->seo_opt['no_dupe']['on'] && !$row['forum_password'] && $auth->acl_get('f_read', $row['forum_id_last_post'])) {
-				$phpbb_seo->prepare_iurl($row, 'topic', $row['topic_type'] == POST_GLOBAL ? $phpbb_seo->seo_static['global_announce'] : $phpbb_seo->seo_url['forum'][$forum_id]);
+			if (!empty($phpbb_seo->seo_opt['no_dupe']['on']) && !$row['forum_password'] && $auth->acl_get('f_read', $row['forum_id_last_post'])) {
+				$phpbb_seo->prepare_iurl($row, 'topic', $row['topic_type'] == POST_GLOBAL ? $phpbb_seo->seo_static['global_announce'] : $phpbb_seo->seo_url['forum'][$row['forum_id_last_post']]);
 				$last_post_url =  append_sid("{$phpbb_root_path}viewtopic.$phpEx", 'f=' . $row['forum_id_last_post'] . '&amp;t=' . $row['topic_id'] . '&amp;start=' . @intval($phpbb_seo->seo_opt['topic_last_page'][$row['topic_id']]) ) . '#p' . $row['forum_last_post_id'];
 				$topic_title = censor_text($row['topic_title']);
 				// Limit in chars for the last post link text.
