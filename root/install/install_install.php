9a508fd5ffa00c7ee1b8cccf279cc78641ff55b6
dcz <dcz@phpbb-seo.com>
2011-06-17 17:16:40 +0000

    PREMOD : 3.0.9-RC2 first merge. Needs testing, commits will follow.

--- root/install/install_install.php
+++ root/install/install_install.php
@@ -113,7 +113,7 @@ class install_install extends module
 				$db_tools = new phpbb_db_tools($db);
 				$db_tools->db->sql_return_on_error(true);
 				$db_tools->sql_column_add(TOPICS_TABLE, 'topic_url', array('VCHAR', ''));
-				set_config('seo_premod_version', '3.0.9-RC1');
+				set_config('seo_premod_version', '3.0.9-RC2');
 				// Remove the lock file
 				@unlink($phpbb_root_path . 'cache/install_lock');
 
