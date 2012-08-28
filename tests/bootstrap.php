9b7b496679305652451d61bb197a4c2d83e64ed8
Carlo <carlo@phpbbitalia.net>
2012-08-28 15:46:56 +0000

    [CI] Some fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,17 +7,18 @@
 *
 */
 define('IN_PHPBB', true);
-
-require_once $phpbb_root_path . 'includes/startup.php';
-require_once $phpbb_root_path . 'includes/constants.php';
-require_once $phpbb_root_path . 'includes/functions.php';
-require_once $phpbb_root_path . 'includes/utf/utf_tools.php';
+$phpbb_root_path = 'premod/root/';
+$phpEx = 'php';
+require_once $phpbb_root_path . 'includes/startup.' . $phpEx;
+require_once $phpbb_root_path . 'includes/constants.' . $phpEx;
+require_once $phpbb_root_path . 'includes/functions.' . $phpEx;
+require_once $phpbb_root_path . 'includes/utf/utf_tools.' . $phpEx;
 
 if (empty($phpbb_seo))
 {
 	if (!class_exists('phpbb_seo'))
 	{
-		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_class.'.$phpEx);
+		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_class.' . $phpEx);
 	}
 
 	$phpbb_seo = new phpbb_seo();
@@ -29,7 +30,7 @@ if (empty($seo_meta))
 {
 	if (!class_exists('seo_meta'))
 	{
-		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_meta.'.$phpEx);
+		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_meta.' . $phpEx);
 	}
 
 	$seo_meta = new seo_meta();
