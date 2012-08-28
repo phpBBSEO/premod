27df26512ff2a630aa3c26c64eb915aec01f8486
Carlo <carlo@phpbbitalia.net>
2012-08-28 15:06:01 +0000

    [CI] Added .travis.yml and PHPUnit configuration/bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+/**
+*
+* @package phpbb_seo_testing
+* @copyright (c) 2006 - 2012 www.phpbb-seo.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+define('IN_PHPBB', true);
+$phpbb_root_path = '../premod/root/';
+$phpEx = 'php';
+require_once $phpbb_root_path . 'includes/startup.php';
+require_once $phpbb_root_path . 'includes/constants.php';
