f8b2072aee258e34cc9447d5d5d60a07588210af
Carlo <carlo@phpbbitalia.net>
2012-08-28 15:14:10 +0000

    [CI] Updated bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,3 +11,25 @@ $phpbb_root_path = '../premod/root/';
 $phpEx = 'php';
 require_once $phpbb_root_path . 'includes/startup.php';
 require_once $phpbb_root_path . 'includes/constants.php';
+
+if (empty($phpbb_seo))
+{
+	if (!class_exists('phpbb_seo'))
+	{
+		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_class.'.$phpEx);
+	}
+
+	$phpbb_seo = new phpbb_seo();
+
+	@define('PHPBB_USE_BOARD_URL_PATH', true);
+}
+
+if (empty($seo_meta))
+{
+	if (!class_exists('seo_meta'))
+	{
+		require($phpbb_root_path . 'phpbb_seo/phpbb_seo_meta.'.$phpEx);
+	}
+
+	$seo_meta = new seo_meta();
+}
