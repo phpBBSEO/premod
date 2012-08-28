f3278582a33972c35485c50b53a9aa8debff77e9
Carlo <carlo@phpbbitalia.net>
2012-08-28 15:19:41 +0000

    [CI] Added first test

---
+++ tests/class_tests.php
@@ -0,0 +1,19 @@
+<?php
+/**
+*
+* @package phpbb_seo_testing
+* @copyright (c) 2006 - 2012 www.phpbb-seo.com
+* @license http://opensource.org/licenses/gpl-license.php GNU Public License v2
+*
+*/
+
+class class_tests extends PHPUnit_Framework_TestCase
+{
+	function drop_sid_test()
+	{
+		$url_no_sid = 'http://www.phpbb-seo.com/en/site/phpbb-seo-rules-t7.html';
+		$sid = '?sid=TheSidIsHere';
+
+		$this->assertEquals($url_no_sid, $phpbb_seo->drop_sid($url_no_sid . $sid));
+	}
+}
