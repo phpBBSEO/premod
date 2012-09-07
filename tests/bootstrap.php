a3a4819bd9a249d75a5131294a26efbf892857f5
Carlo <carlo@phpbbitalia.net>
2012-09-07 08:50:04 +0000

    [CI] Fixed $phpEx var

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -14,8 +14,8 @@ require_once $phpbb_root_path . 'includes/startup.php';
 
 $table_prefix = 'phpbb_';
 require_once $phpbb_root_path . 'includes/constants.php';
-require_once $phpbb_root_path . 'includes/functions.' . $phpEx;
-require_once $phpbb_root_path . 'includes/utf/utf_tools.' . $phpEx;
+require_once $phpbb_root_path . 'includes/functions.php';
+require_once $phpbb_root_path . 'includes/utf/utf_tools.php';
 
 require_once 'test_framework/phpbb_test_case_helpers.php';
 require_once 'test_framework/phpbb_test_case.php';
